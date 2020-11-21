4007d59981bfd13449af7bd8f256904b212e2343
Brian <briandsalemi@gmail.com>
2020-11-21 11:13:00 -0500

    changed pets route to go to species and id of the pet, added a mounted lifecycle method to capture the current animal and render the animals name on the pet view

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    Pet
+    {{ pet().name }}
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
